54e15d46078bb12bf8e9ab72f81c0821bb1d0455
Arnab <arnab.biswas93@gmail.com>
2022-08-04 09:58:16 -0700

    Added demo for eyelid detection

--- README.md
+++ README.md
@@ -7,8 +7,12 @@ git clone https://github.com/arnabiswas/mediapipe_demo_bdss
 cd mediapipe_demo_bdss
 pip install -r requirements.txt
 ```
+## Demo notebooks
+If you prefer using a jupyter notebook demos checkout mediapipe_eyelid_tutorial.ipynb and mediapipe_hand_tutorial.ipynb
 
-## Hand Detection
+
+## Webcam based demos
+### Hand Detection
 Detects hand landmarks and draws them on the webcam feed.
 You can change the number of hands detected by changing the max_num_hands parameter in the mediapipe_hand.py file.
 See [this](https://google.github.io/mediapipe/solutions/hands.html) for more details.
@@ -18,7 +22,7 @@ To run the demo:
 python mediapipe_hand.py
 ```
 
-## Eyelid Detection
+### Eyelid Detection
 Fits a mesh to your face and draws the eyelid landmarks of the detected face mesh on the webcam feed.
 For more information see [this link](https://google.github.io/mediapipe/solutions/face_mesh).
 
--- mediapipe_eyelids.py
+++ mediapipe_eyelids.py
@@ -6,9 +6,9 @@ mp_face_mesh = mp.solutions.face_mesh
 LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
 RIGHT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ] 
 
-face = cv2.imread('face.jpg')
-cap = cv2.VideoCapture(0)
-ret,frame = cap.read()
+frame = cv2.imread('messi.jpg')
+#cap = cv2.VideoCapture(0)
+#ret,frame = cap.read()
 
 img_h, img_w = frame.shape[:2]
 
@@ -19,21 +19,18 @@ with mp_face_mesh.FaceMesh(
 	min_tracking_confidence=0.5
 ) as face_mesh:
 
-	while True:
-		ret,frame = cap.read()
-		if not ret:
-			break
-		rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-		img_h, img_w = rgb_frame.shape[:2]
-		results = face_mesh.process(rgb_frame)
-		if results.multi_face_landmarks:
-			mesh_points = np.array([np.multiply([p.x, p.y], [img_w, img_h]).astype(int) for p in results.multi_face_landmarks[0].landmark])
-			cv2.polylines(frame, [mesh_points[LEFT_EYE]], True, (0,255,0), 1, cv2.LINE_AA)
-			cv2.polylines(frame, [mesh_points[RIGHT_EYE]], True, (0,255,0), 1, cv2.LINE_AA)
-		cv2.imshow('img', frame)
-		key = cv2.waitKey(1)
-		if key ==ord('q'):
-			break
+while True:
+	rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	img_h, img_w = rgb_frame.shape[:2]
+	results = face_mesh.process(rgb_frame)
+	if results.multi_face_landmarks:
+		mesh_points = np.array([np.multiply([p.x, p.y], [img_w, img_h]).astype(int) for p in results.multi_face_landmarks[0].landmark])
+		cv2.polylines(frame, [mesh_points[LEFT_EYE]], True, (0,255,0), 1, cv2.LINE_AA)
+		cv2.polylines(frame, [mesh_points[RIGHT_EYE]], True, (0,255,0), 1, cv2.LINE_AA)
+	cv2.imshow('img', frame)
+	key = cv2.waitKey(1)
+	if key ==ord('q'):
+		break
 
 cap.release()
 cv2.destroyAllWindows()
